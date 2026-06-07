dacc8e16b3e8fb9226b044f7e3badd85d8487ea6
polydeuces30 <157980126+polydeuces30@users.noreply.github.com>
2026-06-08 01:39:40 +0700

    Add skipValidation option to createEnv

--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -3,6 +3,7 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
 export const env = createEnv({
+	skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 	server: {
 		DATABASE_URL: z.string().min(1),
 		REDIS_URL: z.string().min(1),
